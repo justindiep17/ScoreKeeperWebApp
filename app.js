e5361a96b13179ef456620e9e4f1c44bc7d1a9fe
OnlyDill <justindiep17@gmail.com>
2021-09-20 00:49:40 -0400

    Commit

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '/views'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, "/public")));
 
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
 
-app.get('/', (req, res) => {
-  res.render('home.ejs')
-})
-
-app.listen(3000, () => {
-  console.log("LISTENING ON PORT 3000")
-})
+app.listen(3000, () => {});
